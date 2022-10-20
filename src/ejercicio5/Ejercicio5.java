25173647c12d62d6fd0c64eb67272cef9fe3a5ab
Alumno <Alumno@CATE-LAB101R10.Cientifica.pe>
2022-10-20 12:20:51 -0500

    hola

---
+++ src/ejercicio5/Ejercicio5.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Ejercicio5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int i=100;
+        
+        
+        while(i>=1){
+            if (i%2!=0)
+              System.out.println(i);
+            i--;
+        }
+        
+    }
+    
+}
